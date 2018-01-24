bc13185b8caf583b252378367e31694bcc6c9085
Nareen <1513449@my.brunel.ac.uk>
2018-01-24 19:37:04 +0000

    User Input
    User Input Screen Design Implemented

--- app/src/main/java/com/brunel/fyp_not_eat/HomeActivity.java
+++ app/src/main/java/com/brunel/fyp_not_eat/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.brunel.fyp_not_eat;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +15,7 @@ import android.view.WindowManager;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +24,20 @@ public class HomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(this, UserInputActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClick1(View v) {
+
+        Intent intent = new Intent(this, UserInputHealthActivity.class);
+        startActivity(intent);
+
     }
 }
