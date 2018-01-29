6a98f52f324dcc9f9f3579a3bd1cfd64729f0070
Nareen <1513449@my.brunel.ac.uk>
2018-01-29 21:52:43 +0000

    Database Integration
    Displayed the list of results from the database

---
+++ app/src/main/java/com/brunel/fyp_not_eat/DisplayActivity.java
@@ -0,0 +1,66 @@
+package com.brunel.fyp_not_eat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DisplayActivity extends AppCompatActivity {
+    static ListView lv;
+    EditText et;
+    TextView t1;
+    String A="";
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    String value;
+    int c=0;
+    String[] whatnottoeat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        lv = (ListView) findViewById(R.id.listview);
+        arrayList = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(DisplayActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(adapter);
+
+                btnclk();
+                lv.setAdapter(adapter);
+                /*lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position,
+                                            long id) {
+                        String item = ((TextView) view).getText().toString();
+                        System.out.println(item+"");
+                    }
+                });*/
+    }
+    public void btnclk() {
+        if (c == 0) {
+            c++;
+            Scanner in = new Scanner(UserInputActivity.whatnottoeat[UserInputActivity.indexx]).useDelimiter("\\,");
+            while (in.hasNext() == true) {
+                String G = in.next();
+                adapter.add(G+"");
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+}
--- app/src/main/java/com/brunel/fyp_not_eat/UserInputActivity.java
+++ app/src/main/java/com/brunel/fyp_not_eat/UserInputActivity.java
@@ -1,5 +1,6 @@
 package com.brunel.fyp_not_eat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,33 +9,31 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-
 public class UserInputActivity extends AppCompatActivity {
     static ListView lv;
-    EditText et;
+    EditText et;static int indexx=0;
+    static  String item="";
     TextView t1;
     String A="";
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
     String value;
-
+    int c=0;
+    static String[] whatnottoeat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
         lv = (ListView) findViewById(R.id.listview);
         arrayList = new ArrayList<String>();
+
         adapter = new ArrayAdapter<String>(UserInputActivity.this, android.R.layout.simple_list_item_1, arrayList);
-        t1.setText("Here1");
         lv.setAdapter(adapter);
         FirebaseDatabase database_b = FirebaseDatabase.getInstance();
         final DatabaseReference myRefb = database_b.getReference();
@@ -45,48 +44,53 @@ public class UserInputActivity extends AppCompatActivity {
                 // whenever data at this location is updated.
                 value = dataSnapshot.getValue().toString();
                 btnclk(value);
-                t1.setText("Add Players");
+                lv.setAdapter(adapter);
                 lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position,
                                             long id) {
-                        String item = ((TextView) view).getText().toString();
-
-                            item = item.trim();
-                            item = item.substring(0, item.indexOf("§"));
-                            item = item.trim();
-                            String A = item.substring(item.indexOf("=") + 1, item.length());
-                            item = item.trim();
-                            String B = item.substring(0, item.indexOf("="));
-                            FirebaseDatabase database_b = FirebaseDatabase.getInstance();
-                            final DatabaseReference myRefb = database_b.getReference("button");
-                            A = A.trim();
-                            B = B.trim();
-                            myRefb.setValue("");
-                            myRefb.push().setValue("§" + B + "§" + A + "§");
-
+                        item = ((TextView) view).getText().toString();
+                        indexx=position;
+                        Intent intent = new Intent(UserInputActivity.this, DisplayActivity.class);
+                        startActivity(intent);
                     }
                 });
-
             }
-
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
             }
         });
     }
-    public void btnclk(String result)
-    {
-        adapter.clear();
-        result=result.substring(1,result.length());
-        Scanner in=new Scanner(result).useDelimiter("\\,");
-        while (in.hasNext()==true)
-        {
-            String G = in.next();
-            adapter.add(G);
+    public void btnclk(String result) {
+        if (c == 0) {
+            c++;
+            int count = 0, num = 0;
+            System.out.println(result);
+            result = result.substring(1, result.length());
+            for (int i = 0; i < result.length(); i++) {
+                Scanner in = new Scanner(result).useDelimiter("\\§");
+                while (in.hasNext() == true) {
+                    String G = in.next();
+                    count++;
+                }
+            }
+            System.out.println(count);
+            whatnottoeat = new String[count];
+            for (int i = 0; i < result.length(); i++) {
+                System.out.println(result);
+                Scanner in = new Scanner(result).useDelimiter("\\§");
+                while (in.hasNext() == true) {
+                    String G = in.next();
+                    System.out.println(G);
+                    if (G.equalsIgnoreCase(null) == false) {
+                        String itemm = G.substring(0, G.indexOf("="));
+                        whatnottoeat[num++] = G.substring(G.indexOf("=") + 1, G.length());
+                        adapter.add(itemm + "");
+                    }
+                }
+            }
             adapter.notifyDataSetChanged();
         }
-        //adapter.notifyDataSetChanged();
     }
 }
